ff33f3e43f7f40d06f8bd0c7aa8d8e3d1c7da0a9
catphone50 <catphone50@gmail.com>
2024-08-28 13:43:48 +0200

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,30 @@
 import globals from "globals";
+import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      quotes: ["error", "double"],
+      "jsx-quotes": ["error", "prefer-double"],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-no-duplicate-props": "error",
+      "react/no-unused-state": "error",
+      "no-undef": "error",
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect", // Автоматически определяет версию React
+      },
+    },
+  },
+  pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
